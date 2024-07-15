3c6bc2dda7f1884bc5b1658106246ebf1cb8426f
Claudio Iury <claudioiury@MacBook-Air-de-IRIS.local>
2024-07-15 20:46:33 -0300

    feat(): adding functions to utils folder

---
+++ src/client/components/molecules/HeroContainer/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { slideInFromTop } from "@/src/client/utils/motion.util";
+
+export default function HeroContainer(): JSX.Element {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+        ></motion.div>
+      </div>
+    </motion.div>
+  );
+}
---
+++ src/client/utils/motion.util.ts
@@ -0,0 +1,41 @@
+import { Variants } from "framer-motion";
+
+export function slideInFromLeft(delay: number): Variants {
+  return {
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export function slideInFromRight(delay: number): Variants {
+  return {
+    hidden: { x: 100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export const slideInFromTop: Variants = {
+  hidden: { y: -100, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+    },
+  },
+};
