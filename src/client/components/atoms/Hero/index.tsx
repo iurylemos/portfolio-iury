9fce27146bf6e6b742fdd593a556ec470466cb57
iurylemos <iurylemos4@gmail.com>
2024-07-15 21:53:39 -0300

    i have been creating the screen dashboard

--- src/client/components/atoms/Hero/index.tsx
+++ src/client/components/atoms/Hero/index.tsx
@@ -20,7 +20,7 @@ export default function Hero(): JSX.Element {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+          className="welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b490ff] mr-[10px] h-5 w-5" />
           <h1 className="welcome-text text-[13px]">
@@ -29,13 +29,16 @@ export default function Hero(): JSX.Element {
         </motion.div>
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          Providing
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            the best
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            project experience
           </span>
-          project experience
         </motion.div>
 
         <motion.p
